ea4d76f05d3cec70f6a66f73a960f449080c1873
mikailov038 <mikailov038@gmail.com>
2022-09-05 18:14:34 +0300

    my first commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private Deth8 death;
+
+    @Autowired
+    public Needle7(Deth8 deth) {
+        this.death = deth;
+    }
 
     @Override
     public String toString() {
